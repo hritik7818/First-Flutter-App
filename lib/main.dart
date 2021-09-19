8e88499482cdc7255c3b734233b04f46568636e3
Hritik Rajput <hritikrajput30501@gmail.com>
2021-09-19 22:49:29 +0530

    adding image, google font and elevated button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import 'package:flutter_application_2/pages/homePage.dart';
 import 'package:flutter_application_2/pages/loginPage.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:google_fonts/google_fonts.dart";
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -6,15 +7,50 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontSize: 60,
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            "assets/images/loginImage.png",
           ),
-        ),
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            "Login",
+            style: GoogleFonts.oswald(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter email",
+                    labelText: "Email",
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter password",
+                    labelText: "password",
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                print("button pressed");
+              },
+              child: Text("login"))
+        ],
       ),
     );
   }
